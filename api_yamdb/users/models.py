df589c8147a340c993b48c75a7e536ab90d8f061
Alexander Davydov <zk31ns@mail.ru>
2025-06-04 00:44:24 +0300

    создан файл users/constants.py, исправил замечания в users/models.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,77 +1,53 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-# Это не универсальный переводчик. Данная функция умеет работать только с заранее заданными фразами.
-# Их можно дополнять, но нет необходимости делать это в рамках учебного проекта.
-# Давайте просто уберем.
 
+from users.constants import LIMIT_EMAIL, LIMIT_USERNAME, OUTPUT_LENGTH
 from api.validators import username_validator
-# Заведем в каждом приложении файл constants.py для хранения констант.
-# Не надо все складывать в settings.py
-from api_yamdb.settings import (
-    LIMIT_CODE, LIMIT_EMAIL, LIMIT_USERNAME, OUTPUT_LENGTH
-)
 
 
 class User(AbstractUser):
-    # Хороший выбор инструмента для создания вариантов значения для поля role
     class Role(models.TextChoices):
-        USER = 'user', _('Пользователь')
-        MODERATOR = 'moderator', _('Модератор')
-        ADMIN = 'admin', _('Администратор')
+        USER = 'user', 'Пользователь'
+        MODERATOR = 'moderator', 'Модератор'
+        ADMIN = 'admin', 'Администратор'
 
     username = models.CharField(
-        _('Имя пользователя'),
+        'Имя пользователя',
         max_length=LIMIT_USERNAME,
         unique=True,
-        help_text=_(
-            'Обязательное поле. Не более %(limit)s символов. '
-            'Только буквы, цифры и @/./+/-/_.'
-        # Используем f-строку для формирования help_text.
-        # Форматирование через % считается устаревшим (наследие второго питона).
-        ) % {'limit': LIMIT_USERNAME},
+        help_text=f'Обязательное поле. Не более {LIMIT_USERNAME} символов. '
+                  f'Только буквы, цифры и @/./+/-/_.',
         validators=[username_validator],
         error_messages={
-            'unique': _('Пользователь с таким именем уже существует!'),
+            'unique': 'Пользователь с таким именем уже существует!',
         },
     )
     email = models.EmailField(
-        _('Электронная почта'),
+        'Электронная почта',
         max_length=LIMIT_EMAIL,
         unique=True,
         error_messages={
-            'unique': _('Пользователь с таким email уже существует!'),
+            'unique': 'Пользователь с таким email уже существует!',
         },
     )
     role = models.CharField(
-        _('Роль'),
-        # Хороший способ задать лимит длины поля.
+        'Роль',
         max_length=max(len(role) for role in Role.values),
         choices=Role.choices,
         default=Role.USER,
     )
     bio = models.TextField(
-        _('Биография'),
+        'Биография',
         blank=True,
-        help_text=_('Расскажите немного о себе')
-    )
-    # Лишнее поле. Вы используете default_token_generator. Этот инструмент позволяет не хранить данные в БД.
-    # Механизм схож с механизмом JWT-токенов - в токен зашифровывается часть данных пользователя
-    # (его id) и некоторая мета-информация. Проверка валидности осуществляется через default_token_generator.check_token
-    confirmation_code = models.CharField(
-        _('Код подтверждения'),
-        max_length=LIMIT_CODE,
-        blank=True,
-        null=True,
-        help_text=_('Код для подтверждения регистрации')
+        help_text='Расскажите немного о себе'
     )
     first_name = models.CharField(
-        _('Имя'),
+        'Имя',
         max_length=LIMIT_USERNAME,
         blank=True
     )
     last_name = models.CharField(
-        _('Фамилия'),
+        'Фамилия',
         max_length=LIMIT_USERNAME,
         blank=True
     )
@@ -84,7 +60,6 @@ class User(AbstractUser):
         return self.role == self.Role.MODERATOR
 
     @property
-    # Супер!
     def is_admin(self):
         return (
             self.role == self.Role.ADMIN
@@ -93,8 +68,8 @@ class User(AbstractUser):
         )
 
     class Meta:
-        verbose_name = _('Пользователь')
-        verbose_name_plural = _('Пользователи')
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
         ordering = ('username',)
 
     def __str__(self):
